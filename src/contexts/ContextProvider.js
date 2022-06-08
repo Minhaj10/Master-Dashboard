a2ece4103b038aa01bed6c25779d10a477c0516a
Minhaj ahmed <43043473+Minhaj10@users.noreply.github.com>
2022-06-08 11:10:52 +0600

    context implemented

---
+++ src/contexts/ContextProvider.js
@@ -0,0 +1,29 @@
+import React,{createContext, useContext, useState} from "react";
+
+
+const stateContext= createContext();
+
+const intialState = {
+    chat : false,
+    cart : false,
+    userProfile : false,
+    notification : false,
+}
+
+export const ContextProvider = ({children})=>{
+    
+    const [activeMenu, setactiveMenu] = useState(true);
+
+    
+    return(
+        <stateContext.Provider value={
+            {
+                activeMenu,
+            }
+        }>
+         {children}
+        </stateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(stateContext);
