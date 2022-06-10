af57f06068eddeeefd39311d458a1a0d08cc5407
Minhaj ahmed <43043473+Minhaj10@users.noreply.github.com>
2022-06-10 19:51:59 +0600

    banner
    ecommerce page banner added

--- src/App.js
+++ src/App.js
@@ -61,32 +61,35 @@ const App = () => {
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar />
             </div>
-          </div>
 
-          <div>
-            <Routes>
-              <Route path="/" element={<Ecommerce />}></Route>
-              <Route path="/ecommerce" element={<Ecommerce />}></Route>
+            <div>
+              <Routes>
+                <Route path="/" element={<Ecommerce />}></Route>
+                <Route path="/ecommerce" element={<Ecommerce />}></Route>
 
-              <Route path="/orders" element={<Orders />}></Route>
-              <Route path="/employees" element={<Employees />}></Route>
-              <Route path="/orders" element={<Orders />}></Route>
-              <Route path="/customers" element={<Customers />}></Route>
+                <Route path="/orders" element={<Orders />}></Route>
+                <Route path="/employees" element={<Employees />}></Route>
+                <Route path="/orders" element={<Orders />}></Route>
+                <Route path="/customers" element={<Customers />}></Route>
 
-              <Route path="/kanban" element={<Kanban />}></Route>
-              <Route path="/editor" element={<Editor />}></Route>
-              <Route path="/calendar" element={<Calendar />}></Route>
-              <Route path="/colour-picker" element={<ColorPicker />}></Route>
+                <Route path="/kanban" element={<Kanban />}></Route>
+                <Route path="/editor" element={<Editor />}></Route>
+                <Route path="/calendar" element={<Calendar />}></Route>
+                <Route path="/colour-picker" element={<ColorPicker />}></Route>
 
-              <Route path="/line" element={<Line />}></Route>
-              <Route path="/area" element={<Area />}></Route>
-              <Route path="/bar" element={<Bar />}></Route>
-              <Route path="/pie" element={<Pie />}></Route>
-              <Route path="/financial" element={<Financial />}></Route>
-              <Route path="/colour-mapping" element={<ColorMapping />}></Route>
-              <Route path="/pyramid" element={<Pyramid />}></Route>
-              <Route path="/stacked" element={<Stacked />}></Route>
-            </Routes>
+                <Route path="/line" element={<Line />}></Route>
+                <Route path="/area" element={<Area />}></Route>
+                <Route path="/bar" element={<Bar />}></Route>
+                <Route path="/pie" element={<Pie />}></Route>
+                <Route path="/financial" element={<Financial />}></Route>
+                <Route
+                  path="/colour-mapping"
+                  element={<ColorMapping />}
+                ></Route>
+                <Route path="/pyramid" element={<Pyramid />}></Route>
+                <Route path="/stacked" element={<Stacked />}></Route>
+              </Routes>
+            </div>
           </div>
         </div>
       </BrowserRouter>
