0af18f5ed2856a74abe1e7f9e10993080f5e86dd
Minhaj ahmed <43043473+Minhaj10@users.noreply.github.com>
2022-06-11 15:34:56 +0600

    area chart implemented

--- src/pages/charts/Area.jsx
+++ src/pages/charts/Area.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  SplineAreaSeries,
+  DateTime,
+  Legend,
+} from "@syncfusion/ej2-react-charts";
+
+import {
+  areaCustomSeries,
+  areaPrimaryXAxis,
+  areaPrimaryYAxis,
+} from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
 
 const Area = () => {
-  return (
-    <div>Area</div>
-  )
-}
+  const { currentMode } = useStateContext();
 
-export default Area
+  return (
+    <ChartComponent
+      id="area-chart"
+      height="420px"
+      primaryXAxis={areaPrimaryXAxis}
+      primaryYAxis={areaPrimaryYAxis}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      background={currentMode === "Dark" ? "#33373E" : "#fff"}
+      legendSettings={{ background: "white" }}
+    >
+      <Inject services={[SplineAreaSeries, DateTime, Legend]} />
+      <SeriesCollectionDirective>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {areaCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
+};
+
+export default Area;
