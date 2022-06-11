c5ee0e9b27495ebbd814c8a082ee0286437d5557
Minhaj ahmed <43043473+Minhaj10@users.noreply.github.com>
2022-06-11 16:15:18 +0600

    stacked implemented

--- src/pages/charts/Bar.jsx
+++ src/pages/charts/Bar.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  BarSeries,
+  DataLabel,
+  Category,
+  Legend,
+} from "@syncfusion/ej2-react-charts";
+
+import {
+  barCustomSeries,
+  barPrimaryXAxis,
+  barPrimaryYAxis,
+} from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
+import { Tooltip } from "@syncfusion/ej2-react-popups";
+import { IoLogoPolymer } from "react-icons/io";
 
 const Bar = () => {
-  return (
-    <div>Bar</div>
-  )
-}
+  const { currentMode } = useStateContext();
 
-export default Bar
+  return (
+    <ChartComponent
+      id="charts"
+      primaryXAxis={barPrimaryXAxis}
+      primaryYAxis={barPrimaryYAxis}
+
+      // background={currentMode === "Dark" ? "#33373E" : "#fff"}
+      // // legendSettings={{ background: "white" }}
+    >
+      <Inject services={[BarSeries, Legend, Tooltip, DataLabel, Category]} />
+      <SeriesCollectionDirective>
+        {barCustomSeries.map((item) => (
+          <SeriesDirective
+            dataSource={item.dataSource}
+            xName={item.xName}
+            yName={item.yName}
+            name={item.name}
+            type={item.type}
+          />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
+};
+
+export default Bar;
