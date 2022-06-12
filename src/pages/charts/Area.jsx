b6e3674ae87affac31c09dcb895ccf8b2ec48895
Minhaj ahmed <43043473+Minhaj10@users.noreply.github.com>
2022-06-12 12:15:02 +0600

    Update Area.jsx
    area chart bug fixed ,added headline

--- src/pages/charts/Area.jsx
+++ src/pages/charts/Area.jsx
@@ -4,11 +4,12 @@ import {
   SeriesCollectionDirective,
   SeriesDirective,
   Inject,
-  SplineAreaSeries,
   DateTime,
+  SplineAreaSeries,
   Legend,
 } from "@syncfusion/ej2-react-charts";
 
+import { ChartsHeader } from "../../components";
 import {
   areaCustomSeries,
   areaPrimaryXAxis,
@@ -20,24 +21,27 @@ const Area = () => {
   const { currentMode } = useStateContext();
 
   return (
-    <ChartComponent
-      id="area-chart"
-      height="420px"
-      primaryXAxis={areaPrimaryXAxis}
-      primaryYAxis={areaPrimaryYAxis}
-      chartArea={{ border: { width: 0 } }}
-      tooltip={{ enable: true }}
-      background={currentMode === "Dark" ? "#33373E" : "#fff"}
-      legendSettings={{ background: "white" }}
-    >
-      <Inject services={[SplineAreaSeries, DateTime, Legend]} />
-      <SeriesCollectionDirective>
-        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        {areaCustomSeries.map((item, index) => (
-          <SeriesDirective key={index} {...item} />
-        ))}
-      </SeriesCollectionDirective>
-    </ChartComponent>
+    <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <ChartsHeader category="Area" title="Inflation Rate in percentage" />
+      <div className="w-full">
+        <ChartComponent
+          id="charts"
+          primaryXAxis={areaPrimaryXAxis}
+          primaryYAxis={areaPrimaryYAxis}
+          chartArea={{ border: { width: 0 } }}
+          background={currentMode === "Dark" ? "#33373E" : "#fff"}
+          legendSettings={{ background: "white" }}
+        >
+          <Inject services={[SplineAreaSeries, DateTime, Legend]} />
+          <SeriesCollectionDirective>
+            {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+            {areaCustomSeries.map((item, index) => (
+              <SeriesDirective key={index} {...item} />
+            ))}
+          </SeriesCollectionDirective>
+        </ChartComponent>
+      </div>
+    </div>
   );
 };
 
