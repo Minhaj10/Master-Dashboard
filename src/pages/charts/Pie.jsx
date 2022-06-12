9ba72aabd69f348c7b728067a002abb811201171
Minhaj ahmed <43043473+Minhaj10@users.noreply.github.com>
2022-06-12 12:04:38 +0600

    pie chart implemented

--- src/pages/charts/Pie.jsx
+++ src/pages/charts/Pie.jsx
@@ -1,39 +1,20 @@
 import React from "react";
-import {
-  AccumulationChartComponent,
-  AccumulationSeriesCollectionDirective,
-  AccumulationSeriesDirective,
-  AccumulationLegend,
-  PieSeries,
-  AccumulationDataLabel,
-  Inject,
-  AccumulationTooltip,
-} from "@syncfusion/ej2-react-charts";
 
-import {
-  pieChartData,
-  LinePrimaryXAxis,
-  LinePrimaryYAxis,
-} from "../../data/dummy";
+import { pieChartData } from "../../data/dummy";
+import { ChartsHeader, Pie as PieChart } from "../../components";
 
-import { useStateContext } from "../../contexts/ContextProvider";
-
-const Pie = ({ id, data, legendVisiblity, height }) => {
-  const { currentMode } = useStateContext();
-
-  return (
-    <AccumulationChartComponent id="charts">
-      <Inject services={[PieSeries]} />
-      <AccumulationSeriesCollectionDirective>
-        <AccumulationSeriesDirective
-          dataSource={pieChartData}
-          xName="x"
-          yName="y"
-          type="Pie"
-        ></AccumulationSeriesDirective>
-      </AccumulationSeriesCollectionDirective>
-    </AccumulationChartComponent>
-  );
-};
+const Pie = () => (
+  <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+    <ChartsHeader category="Pie" title="Project Cost Breakdown" />
+    <div className="w-full">
+      <PieChart
+        id="chart-pie"
+        data={pieChartData}
+        legendVisiblity
+        height="full"
+      />
+    </div>
+  </div>
+);
 
 export default Pie;
