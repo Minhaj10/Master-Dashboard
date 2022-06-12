e810cbefbce528acf318868297d84148b3a3afb5
Minhaj ahmed <43043473+Minhaj10@users.noreply.github.com>
2022-06-12 19:57:33 +0600

    updated tab and sidebar icon

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,6 +4,7 @@ import { SiShopware } from "react-icons/si";
 import { MdOutlineCancel } from "react-icons/md";
 import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
+import { BiPaperPlane } from "react-icons/bi";
 import { links } from "../data/dummy";
 import { useStateContext } from "../contexts/ContextProvider";
 
@@ -32,7 +33,7 @@ const Sidebar = () => {
               onClick={handleCloseSideBar}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
-              <SiShopware /> <span>Shoppy</span>
+              <BiPaperPlane /> <span>Master Dashboard</span>
             </Link>
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
